eb89085b23972255a776e0c6272f8b5f0275da1b
Popoola <popoolso@ucmail.uc.edu>
2024-09-16 12:07:17 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+public class HelloWorld {
+    public static void main (String [] args){
+        System.out.println("Hello World");
+        int sum = 5 / 2;
+        //int sum_d = 5.0 / 2;
+        int sum_d2 = (int) 5.0 /2;
+        double sum_double = 5/2;
+        double sum_double2 = 5.0/2;
+
+        System.out.println("5/2 is "+ sum);
+        System.out.println("int 5.0/ 2  is "+ sum_d2);
+        System.out.println("double 5/2 is " + sum_double);
+        System.out.println("double 5.0/2 is "+ sum_double2);
+
+        double sqrt = Math.sqrt(9);
+        double pow = Math.pow(2,3);
+        System.out.println(sqrt);
+        System.out.println(pow);
+
+
+
+
+        //sout
+    }
+
+}
